1f6f01d734b9ba7204e4f174ebc888c6a1ece220
David Huang <david10208.dw@gmail.com>
2024-01-12 23:00:42 +0100

    Initial Commit

---
+++ openglProj/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  openglProj
+//
+//  Created by David Wong on 12.01.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
